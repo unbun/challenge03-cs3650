9f94046fd3461a6377ea8e18db71651a1b2e39d9
Noel Prince <prince.no@husky.neu.edu>
2020-04-10 12:52:40 -0400

    My implementation

---
+++ homework-10/inode.h
@@ -0,0 +1,20 @@
+#ifndef INODE_H
+#define INODE_H
+
+#include "pages.h"
+
+typedef struct inode {
+    int refs; // reference count
+    int mode; // permission & type
+    int size; // bytes
+    int ptrs[2]; // direct pointers
+    int iptr; // single indirect pointer
+} inode;
+
+void print_inode(inode* node);
+inode* get_inode(int inum);
+int alloc_inode();
+void free_inode();
+int inode_get_pnum(inode* node, int fpn);
+
+#endif
